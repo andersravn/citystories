e2e3dbde3ddc8eac14ce8df684baef14be713111
Anders Ravn <aravn4@gmail.com>
2015-08-27 13:24:04 +0200

    Takes differing son fields into account

--- citystories/api/controllers.py
+++ citystories/api/controllers.py
@@ -4,7 +4,16 @@ import requests
 def get_address(lat, lon):
     response = requests.get('http://nominatim.openstreetmap.org/reverse?lat=' + lat + '&lon=' + lon + '&format=json')
     data = response.json()
-    street = data['address']['road']
+
+    # Not all responses has a road field, some might have a field called pedestrian instead.
+    try:
+        street = data['address']['road']
+    except KeyError:
+        try:
+            street = data['address']['pedestrian']
+        except KeyError:
+            street = ''
+
     # Not all responses has a house_number field.
     try:
         number = data['address']['house_number']
